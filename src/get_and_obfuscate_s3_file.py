0da5305cb436aafb0a726820bbb81b069221285b
Padma Priya <padmapriya.mariappan@gmail.com>
2024-11-27 17:04:49 +0000

    Change to test the inputs and file types to obfuscate function and relevant tests

--- src/get_and_obfuscate_s3_file.py
+++ src/get_and_obfuscate_s3_file.py
@@ -1,16 +1,31 @@
-import boto3
-from read_s3_object_to_dataframe import read_s3_object_into_dataframe
+# from read_s3_object_to_dataframe import read_s3_object_into_dataframe
+
+supported_file_types = ['csv', 'json', 'parquet']
+class InvalidInputError(Exception):  
+    """  
+    Raised when the input file/fields are not valid 
+    """  
+    pass 
+
+class UnsupportedFileTypeError(Exception):  
+    """  
+    Raised when the input file type is not supported by obfuscator
+    """  
+    pass 
 
 def get_and_obfuscate_s3_file(s3_details: dict):
 
-    if s3_details['pii_fields'] is None:
-        raise TypeError("No fields to obfuscate")
+    if not s3_details['pii_fields'] or s3_details['file_to_obfuscate'] is None:
+        raise InvalidInputError("No file/fields provided to obfuscate")
  
     bucket_name = s3_details['file_to_obfuscate'].split('/')[-2]
     s3_file = s3_details['file_to_obfuscate'].split('/')[-1]
-    print(bucket_name)
-    print(s3_file)
-    print(s3_details['pii_fields'])
+
+    file_type = s3_file.split('.')[-1]
+
+    if file_type not in supported_file_types:
+        raise UnsupportedFileTypeError("Only csv, json and parquet files supported for obfuscation")
+
     
     # try:
     #     s3_data_frame = read_s3_object_into_dataframe(bucket_name, s3_file)
