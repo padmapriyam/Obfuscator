7ba15bec9cb7b5001fff914006bd98d7ede7a5df
Padma Priya <padmapriya.mariappan@gmail.com>
2024-12-02 16:55:25 +0000

    Add function to retrieve the file from s3 and relevant tests

---
+++ src/read_s3_object_into_dataframe.py
@@ -0,0 +1,37 @@
+import boto3
+import pandas as pd
+import io
+
+def read_s3_object_into_dataframe(bucket_name, key, type_of_file):
+    """This function is used to read csv file in s3 bucket and convert into a dataframe, ready for obfuscating the fields.
+
+    Args:
+        bucket_name: the name of the bucket to access tables containing json lines data from, in this case, the imgestion s3 bucket.
+        key: the filepath within the bucket to the json lines file to be read into a dataframe.
+        type_of_file: the type of the input file retrieved from the key passed
+
+    Returns:
+        A dataframe of the records in the input file.
+
+    Raises:
+        TyperError is the bucket_name or key arguments passed are not strings, type of file not supported.
+    """
+    supported_file_types = ['csv', 'json', 'parquet']
+
+    if not isinstance(bucket_name, str):
+        raise TypeError("bucket_name must be a string")
+
+    if not isinstance(key, str):
+        raise TypeError("the specified key must be a string")
+    
+    if type_of_file not in supported_file_types:
+        raise TypeError("Only csv, json and parquet files supported")
+
+    try:
+        s3_client = boto3.client("s3")
+        response = s3_client.get_object(Bucket=bucket_name, Key=key)
+        body = response["Body"].read().decode('utf-8')
+        df = pd.read_csv(io.StringIO(body), index_col=[0])
+        return df
+    except Exception as e:
+        raise ValueError(f"Error reading or processing the object from S3: {e}")
---
+++ tests/test_read_s3_object_into_dataframe.py
@@ -0,0 +1,81 @@
+import pytest
+import pandas as pd 
+import boto3
+from moto import mock_aws
+import os
+from src.read_s3_object_into_dataframe import read_s3_object_into_dataframe
+
+@pytest.fixture(scope="function")
+def aws_creds():
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture
+def s3_client(aws_creds):
+    with mock_aws():
+        yield boto3.client("s3")
+
+
+@pytest.fixture
+def s3_bucket(s3_client):
+    bucket_name = "test-bucket"
+    region = "eu-west-2"
+    bucket = s3_client.create_bucket(
+        Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": region}
+    )
+    return bucket_name
+
+@pytest.fixture
+def add_bucket_object(s3_client, s3_bucket):
+    
+    df = pd.DataFrame({
+        "id" : ["1", "2"],
+        "name" : ["person1", "person2"],
+        "country" : ["UK", "India"]
+    })
+
+    df_csv = df.to_csv()
+
+    key = "people.csv"
+    s3_client.put_object(Bucket=s3_bucket, Key=key, Body=df_csv)
+    return key
+
+def test_read_s3_object_into_dataframe_returns_a_dataframe(add_bucket_object, s3_bucket):
+
+    key = add_bucket_object
+    df = read_s3_object_into_dataframe(s3_bucket, key, "csv")
+
+    assert isinstance(df, pd.DataFrame)
+
+def test_read_s3_object_into_dataframe_returns_correct_columns(add_bucket_object, s3_bucket):
+
+    key = add_bucket_object
+    df = read_s3_object_into_dataframe(s3_bucket, key, "csv")
+
+    assert list(df.columns) == ["id", "name", "country"]
+    assert len(df) == 2
+
+def test_read_s3_object_into_dataframe_returns_error_when_key_not_found(s3_bucket):
+
+    with pytest.raises(ValueError):
+        read_s3_object_into_dataframe(s3_bucket, "hello", "csv")
+
+def test_read_s3_object_into_dataframe_returns_error_when_no_bucket():
+
+    with pytest.raises(ValueError):
+        read_s3_object_into_dataframe("1", "2", "csv")
+
+def test_read_s3_object_into_dataframe_returns_message_when_not_str():
+
+    with pytest.raises(TypeError):
+        read_s3_object_into_dataframe(1, 2)
+
+def test_read_s3_object_into_dataframe_returns_message_for_unsupported_file_type(add_bucket_object, s3_bucket):
+
+    key = add_bucket_object
+    with pytest.raises(TypeError):
+        read_s3_object_into_dataframe(s3_bucket, key, "txt")
